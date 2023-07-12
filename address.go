4dff9bb70937e46279ab500455fc9b193bdcaa8b
Dennis Waßenberg <development@dewaco.de>
2023-07-12 11:21:37 +0200

    Add field `AddressAddition` in Address

--- address.go
+++ address.go
@@ -8,18 +8,19 @@ const (
 )
 
 type Address struct {
-	ID           int             `json:"id,omitempty"`
-	Company      string          `json:"company,omitempty"`   // Name 1
-	FirstName    string          `json:"firstName,omitempty"` // Name 2
-	LastName     string          `json:"lastName,omitempty"`  // Name 3
-	Street       string          `json:"street,omitempty"`
-	StreetNumber string          `json:"streetNumber,omitempty"`
-	PostalCode   string          `json:"postalCode,omitempty"`
-	City         string          `json:"city,omitempty"`
-	Country      string          `json:"country,omitempty"`
-	State        string          `json:"state,omitempty"`
-	Mail         string          `json:"mail,omitempty"`
-	Phone        string          `json:"phone,omitempty"`
-	VATNumber    string          `json:"vatNumber,omitempty"`
-	AddressType  AddressTypeCode `json:"addressType,omitempty"`
+	ID              int             `json:"id,omitempty"`
+	Company         string          `json:"company,omitempty"`   // Name 1
+	FirstName       string          `json:"firstName,omitempty"` // Name 2
+	LastName        string          `json:"lastName,omitempty"`  // Name 3
+	Street          string          `json:"street,omitempty"`
+	StreetNumber    string          `json:"streetNumber,omitempty"`
+	PostalCode      string          `json:"postalCode,omitempty"`
+	AddressAddition string          `json:"addressAddition,omitempty"`
+	City            string          `json:"city,omitempty"`
+	Country         string          `json:"country,omitempty"`
+	State           string          `json:"state,omitempty"`
+	Mail            string          `json:"mail,omitempty"`
+	Phone           string          `json:"phone,omitempty"`
+	VATNumber       string          `json:"vatNumber,omitempty"`
+	AddressType     AddressTypeCode `json:"addressType,omitempty"`
 }
